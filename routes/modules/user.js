63cb12e75c62d235a32480e56c75f73afb9c550c
goodjobhot401 <goodjobhot@gmail.com>
2023-05-04 17:40:01 +0800

    feat: add register page

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+// 登入
 router.get('/login', (req, res) => {
   res.render('login')
 })
+
+router.post('/login', (req, res) => {
+
+})
+
+// 註冊
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+// router.post('/register', (req, res) => {
+//   res.render
+// })
+
+// 登出
 
 module.exports = router
